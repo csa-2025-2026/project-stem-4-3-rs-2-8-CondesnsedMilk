0afcecd4f1a5bda28b5aebd82cca86facc5a8e0d
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-10-21 21:46:16 +0000

    Almost done

--- Main.java
+++ Main.java
@@ -4,6 +4,45 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    /*
+    for(int i = 1; i <= 25; i += 2)
+    {
+      System.out.println(i);
+    }
+    */
+
+    /*
+    for(i = 17; i <= 73; i++)
+    {
+      System.out.print(i + " ");
+      if(i % 10 == 6)
+      {
+        System.out.println();
+      }
+      
+    }
+    */
+
+    System.out.print("Etner in a number between 0 and 50: ");
+    Scanner scan = new Scanner(System.in);
+    int scannedInt = scan.nextInt();
+
+    if(scannedInt < 0 || scannedInt > 50)
+    {
+      System.out.println("error");
+    }
+    else
+    {
+      for(int i = scannedInt; i <= 50; i++)
+      {
+        System.out.print(i + " ");
+        if(i % 5 == (scannedInt+4) % 5)
+        {
+          System.out.println();
+        }
+
+      }
+    }
+
   }
 }
